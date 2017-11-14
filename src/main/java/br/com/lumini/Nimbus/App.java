30cbb6021781b0a38273d704f2682768b9cb38cd
Eurique <jecrjunior@dcomp.ufs.br>
2017-11-14 20:51:02 +0000

    Git configurado

---
+++ src/main/java/br/com/lumini/Nimbus/App.java
@@ -0,0 +1,13 @@
+package br.com.lumini.Nimbus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
